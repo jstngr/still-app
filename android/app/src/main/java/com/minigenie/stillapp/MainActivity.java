83c062bc58eb67da54a1f66b514bcba6e274d768
Jstngr <justi93smail@gmail.com>
2024-08-26 11:54:26 +0200

    Add capacitor

---
+++ android/app/src/main/java/com/minigenie/stillapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.minigenie.stillapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.minigenie.stillapp',
+  appName: 'MiniGenie - Still App',
+  webDir: 'dist'
+};
+
+export default config;
