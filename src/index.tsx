cffc7ec4d154d6cd67d33ec1b40ce396a2a9b4e7
Jstngr <justi93smail@gmail.com>
2024-12-27 20:11:09 +0100

    Add jeepSqlite

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import App from 'App';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { defineCustomElements as jeepSqlite } from 'jeep-sqlite/loader';
+import { Capacitor } from '@capacitor/core';
 
 import 'normalize.css';
 import '@fontsource/poppins';
@@ -9,7 +11,7 @@ import '@fontsource-variable/jetbrains-mono';
 import '@fontsource/dancing-script';
 import './index.css';
 console.log('AAAAAAAAAAAAAAAABBBBBB');
-// import './i18n';
+import './i18n';
 
 console.log('AAAAAAAAAAAAAAAA');
 
@@ -19,10 +21,10 @@ import '@mantine/carousel/styles.css';
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 
-// // Initialize custom elements
-// if (!Capacitor.isNativePlatform()) {
-//   jeepSqlite(window);
-// }
+// Initialize custom elements
+if (!Capacitor.isNativePlatform()) {
+  jeepSqlite(window);
+}
 
 root.render(
   <BrowserRouter>
